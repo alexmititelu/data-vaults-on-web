f6ad627ff6d4ab2c043e7f848662cf2f56e24c0c
gabrielxzc <codrincojocaru@yahoo.com>
2019-01-12 17:25:25 +0200

    Web server now serves static content, firebase initialization

--- application/application.py
+++ application/application.py
@@ -1,6 +1,14 @@
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+
+@app.route('/', methods=['GET'])
+def index():
+    return app.send_static_file('index.html')
+
+
+@app.route('/<path:path>')
+def serve_static(path):
+    return app.send_static_file(path)
--- application/static/assets/scripts/main.js
+++ application/static/assets/scripts/main.js
@@ -1,4 +1,12 @@
-import {test} from './auth.mjs'
+var config = {
+    apiKey: "AIzaSyAHmbjjMt8WdwINBF3lY63DroVJgQ3GBcg",
+    authDomain: "data-vaults-on-web.firebaseapp.com",
+    databaseURL: "https://data-vaults-on-web.firebaseio.com",
+    projectId: "data-vaults-on-web",
+    storageBucket: "data-vaults-on-web.appspot.com",
+    messagingSenderId: "455933671072"
+};
 
+firebase.initializeApp(config);
 
-test()
+console.log("Firebase successfully initialized!")

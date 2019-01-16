2adad3569f7cdb3752444346bd43a2654b3f6d30
gabrielxzc <codrincojocaru@yahoo.com>
2019-01-16 15:46:19 +0200

    javascript code client will use to secure forms

--- application/static/assets/scripts/crypto/crypto-utils.js
+++ application/static/assets/scripts/crypto/crypto-utils.js
@@ -19,7 +19,6 @@ class CryptoUtils {
     newRsaPair(callback) {
         window.crypto.subtle.generateKey({
                 name: "RSA-OAEP",
-                // Consider using a 4096-bit key for systems that require long-term security
                 modulusLength: 4096,
                 publicExponent: new Uint8Array([1, 0, 1]),
                 hash: "SHA-256",
